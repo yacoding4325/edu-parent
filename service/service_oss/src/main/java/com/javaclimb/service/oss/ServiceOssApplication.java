ab0ab1c5a7cd1881d9068f0e7975a9ddcc5eabde
yxym0203 <1500081802@qq.com>
2022-11-01 22:14:54 +0800

    nacos 服务注册及启动

--- service/service_oss/src/main/java/com/javaclimb/service/oss/ServiceOssApplication.java
+++ service/service_oss/src/main/java/com/javaclimb/service/oss/ServiceOssApplication.java
@@ -3,6 +3,7 @@ package com.javaclimb.service.oss;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @ComponentScan({"com.javaclimb"})
+@EnableDiscoveryClient
 public class ServiceOssApplication {
 
     public static void main(String[] args) {
