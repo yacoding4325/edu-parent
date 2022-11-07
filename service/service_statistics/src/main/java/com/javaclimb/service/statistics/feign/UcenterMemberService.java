b1631b7fd0393268674ba8428ab6a4eae1790eb9
yxym0203 <1500081802@qq.com>
2022-11-07 22:01:00 +0800

    在service_ucenter模块创建接口，统计某一天的注册人数; 在service_statistics模块创建远程调用接口; 在service_statistics模块调用微服务

--- service/service_statistics/src/main/java/com/javaclimb/service/statistics/feign/UcenterMemberService.java
+++ service/service_statistics/src/main/java/com/javaclimb/service/statistics/feign/UcenterMemberService.java
@@ -5,5 +5,18 @@ package com.javaclimb.service.statistics.feign;
  * @create 2022-11-06 下午 11:36
  */
 
-public class UcenterMemberService {
+import com.javaclimb.service.base.result.R;
+import com.javaclimb.service.statistics.feign.fallback.UcenterMemberServiceFallBack;
+import io.swagger.annotations.ApiParam;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * 远程调用用户相关的微服务
+ */
+@FeignClient(value = "service-ucenter",fallback = UcenterMemberServiceFallBack.class)
+public interface UcenterMemberService {
+    @GetMapping("/admin/ucenter/member/count-register/{day}")
+    public R countRegisterNum(@ApiParam(value = "统计日期",required = true)@PathVariable String day);
 }
--- service/service_statistics/src/main/java/com/javaclimb/service/statistics/feign/fallback/UcenterMemberServiceFallBack.java
+++ service/service_statistics/src/main/java/com/javaclimb/service/statistics/feign/fallback/UcenterMemberServiceFallBack.java
@@ -1,9 +1,21 @@
 package com.javaclimb.service.statistics.feign.fallback;
 
+import com.javaclimb.service.base.result.R;
+import com.javaclimb.service.statistics.feign.UcenterMemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
 /**
  * @Author yaCoding
  * @create 2022-11-06 下午 11:36
  */
-
-public class UcenterMemberServiceFallBack {
+//熔断类
+@Service
+@Slf4j
+public class UcenterMemberServiceFallBack implements UcenterMemberService {
+    @Override
+    public R countRegisterNum(String day) {
+        log.info("熔断被执行");
+        return R.ok().data("registerNum",0);
+    }
 }
