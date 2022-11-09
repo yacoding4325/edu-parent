e509d1ea303e7fc0b5a4887668262eff5f9306da
yxym0203 <1500081802@qq.com>
2022-11-09 17:19:48 +0800

    密码加密处理类

--- common/spring_security/src/main/java/com/javaclimb/security/security/DefaultPasswordEncoder.java
+++ common/spring_security/src/main/java/com/javaclimb/security/security/DefaultPasswordEncoder.java
@@ -1,9 +1,27 @@
 package com.javaclimb.security.security;
 
+import com.javaclimb.service.base.utils.MD5;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
 /**
  * @Author yaCoding
  * @create 2022-11-09 下午 2:43
  */
 //密码处理类
-public class DefaultPasswordEncoder {
+@Component
+public class DefaultPasswordEncoder implements PasswordEncoder {
+
+    /**加密密码* @return*/
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return MD5.encrypt(rawPassword.toString());
+    }
+
+
+    /** 判断密码 是否正确*/
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return encodedPassword.equals(MD5.encrypt(rawPassword.toString()));
+    }
 }
